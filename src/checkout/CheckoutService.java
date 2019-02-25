87ffdde573793ff757871e9c74226063703700e6
Pifagor <zheka.rachkovan@gmail.com>
2019-02-25 19:42:53 +0300

    complete quest

--- src/checkout/CheckoutService.java
+++ src/checkout/CheckoutService.java
@@ -23,16 +23,5 @@ public class CheckoutService {
 
     public void useOffer(Offer offer) {
         offer.apply(check);
-        if (offer instanceof FactorByCategoryOffer) {
-            FactorByCategoryOffer fbOffer = (FactorByCategoryOffer) offer;
-            int points = check.getCostByCategory(fbOffer.category);
-            check.addPoints(points * (fbOffer.factor - 1));
-        } else {
-            if (offer instanceof AnyGoodsOffer) {
-                AnyGoodsOffer agOffer = (AnyGoodsOffer) offer;
-                if (agOffer.totalCost <= check.getTotalCost())
-                    check.addPoints(agOffer.points);
-            }
-        }
     }
 }
--- src/checkout/FactorByCategoryOffer.java
+++ src/checkout/FactorByCategoryOffer.java
@@ -11,6 +11,8 @@ public class FactorByCategoryOffer extends Offer {
 
     @Override
     public void apply(Check check) {
+        int points = check.getCostByCategory(category);
+        check.addPoints(points * (factor - 1));
 
     }
 }
